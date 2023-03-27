326cf275d2a35269d4071d1698b01c3abe9ab330
unknowntocka <1067075323@qq.com>
2023-03-27 11:28:15 +0800

    feat: 调试业务错误

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -12,6 +12,7 @@ import {
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { BusinessException } from 'src/common/exceptions/business.exception';
 
 @Controller('user')
 export class UserController {
@@ -39,6 +40,18 @@ export class UserController {
   findError() {
     const a: any = {};
     console.log(a.b.c);
+    return this.userService.findAll();
+  }
+
+  @Get('findBusinessError')
+  @Version([VERSION_NEUTRAL, '1'])
+  findBusinessError() {
+    const a: any = {};
+    try {
+      console.log(a.b.c);
+    } catch (error) {
+      throw new BusinessException('参数错误');
+    }
     return this.userService.findAll();
   }
 
